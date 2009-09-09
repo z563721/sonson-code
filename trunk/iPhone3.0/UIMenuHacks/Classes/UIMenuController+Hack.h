073d143b5f0ce65e74f48adb27ebb4e379c3c835
yoshida.yuichi <yoshida.yuichi@0d0a6a56-2bd1-11de-9cad-a3574e5575ef>
2009-09-09 14:18:03 +0000

    ・メソッドを綺麗にまとめた

--- trunk/iPhone3.0/UIMenuHacks/Classes/UIMenuController+Hack.h
+++ trunk/iPhone3.0/UIMenuHacks/Classes/UIMenuController+Hack.h
@@ -2,19 +2,40 @@
 //  UIMenuController+Hack.h
 //  UIMenuHacks
 //
-//  Created by sonson on 09/09/09.
-//  Copyright 2009 __MyCompanyName__. All rights reserved.
+// The MIT License
+//
+// Copyright (c) 2009 sonson, sonson@Picture&Software
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+// Created by sonson on 09/07/11.
+// Copyright 2009 sonson, sonson@Picture&Software. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 
-extern NSString *kDidDismissedUIMenuController;
-
+// delegate method
 @protocol UIMenuControllerHack <NSObject>
 + (void)replaceCallBarButton:(id)callBarButton action:(SEL)action;
-+ (void)getBacckCallBarButton:(id)callBarButton action:(SEL)action;
 @end
 
+// Undocumented APIs of UICallBar
 @interface NSObject(UICallBar)
 - (BOOL)visible;
 - (BOOL)recentlyFaded;
@@ -22,11 +43,9 @@ extern NSString *kDidDismissedUIMenuController;
 - (id)targetForAction:(SEL)action;
 @end
 
+// Undocumented APIs of UICallBarButton
 @interface NSObject(UICallBarButton)
 - (void)setupWithTitle:(id)title action:(SEL)action;
 - (void)setupWithImage:(id)image action:(SEL)action;
 - (SEL)action;
-@end
-
-@interface UIMenuController(Hack)
 @end
--- trunk/iPhone3.0/UIMenuHacks/Classes/UIMenuController+Hack.m
+++ trunk/iPhone3.0/UIMenuHacks/Classes/UIMenuController+Hack.m
@@ -2,64 +2,59 @@
 //  UIMenuController+Hack.m
 //  UIMenuHacks
 //
-//  Created by sonson on 09/09/09.
-//  Copyright 2009 __MyCompanyName__. All rights reserved.
+// The MIT License
+//
+// Copyright (c) 2009 sonson, sonson@Picture&Software
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+// Created by sonson on 09/07/11.
+// Copyright 2009 sonson, sonson@Picture&Software. All rights reserved.
 //
 
 #import "UIMenuController+Hack.h"
 
-NSMutableArray *targetClassArray = nil;
-
 NSMutableArray *editedButtons = nil;
-
-NSString *kDidDismissedUIMenuController = @"kDidDismissedUIMenuController";
 
 @implementation UIMenuController(Hack)
 
-- (void)checkMethod:(id)callBar {
-	if ([callBar visible] && [callBar recentlyFaded]) {
-		DNSLog(@"visible - recentlyFaded = 1 - 1");
-	}
-	else if (![callBar visible] && [callBar recentlyFaded]) {
-		DNSLog(@"visible - recentlyFaded = 0 - 1");
-	}
-	else if ([callBar visible] && ![callBar recentlyFaded]) {
-		DNSLog(@"visible - recentlyFaded = 1 - 0");
-	}
-	else if (![callBar visible] && ![callBar recentlyFaded]) {
-		DNSLog(@"visible - recentlyFaded = 0 - 0");
-	}
-}
-
-- (void)dump:(id)callBar {
+// Get back the captions with strings which are almost original.
+- (void)getBackCallBarButton:(id)callBarButton action:(SEL)action {
 	DNSLogMethod
-	NSLog(@"UICallBarDelegate %s", class_getName([[callBar delegate] class]));
-	NSArray *buttons = [callBar visibleButtons];
-	if ([buttons count]) {
-		id button = [buttons objectAtIndex:0];
-		SEL action = [button action];
-		id target = [callBar targetForAction:action];
-		NSLog(@"UICallBarButtonDelegate %s %@", class_getName([target class]), NSStringFromSelector(action));
-	}
-}
-
-- (void)getBacckCallBarButton:(id)callBarButton action:(SEL)action {
-	DNSLogMethod
-//	NSLog(@"%@", NSStringFromSelector(action));
 	if (action == @selector(cut:)) {
+		[callBarButton setupWithImage:nil action:action];
 		[callBarButton setupWithTitle:NSLocalizedString(@"Cut", nil) action:action];
 	}
 	else if (action == @selector(copy:)) {
-		NSLog(@"%@", NSStringFromSelector(action));
+		[callBarButton setupWithImage:nil action:action];
 		[callBarButton setupWithTitle:NSLocalizedString(@"Copy", nil) action:action];
 	}
 	else if (action == @selector(paste:)) {
+		[callBarButton setupWithImage:nil action:action];
 		[callBarButton setupWithTitle:NSLocalizedString(@"Paste", nil) action:action];
 	}
 	else if (action == @selector(select:)) {
+		[callBarButton setupWithImage:nil action:action];
 		[callBarButton setupWithTitle:NSLocalizedString(@"Select", nil) action:action];
 	}
 	else if (action == @selector(selectAll:)) {
+		[callBarButton setupWithImage:nil action:action];
 		[callBarButton setupWithTitle:NSLocalizedString(@"Select All", nil) action:action];
 	}
 	else if (action == @selector(_setRtoLTextDirection:)) {
@@ -68,93 +63,38 @@ NSString *kDidDismissedUIMenuController = @"kDidDismissedUIMenuController";
 	}
 }
 
+// Call this method when UICallBar's animation will start appearing and hiding. 
 - (void)calloutBarWillStartAnimation:(id)callBar {
 	DNSLogMethod
-//	[self checkMethod:callBar];
-//	[self dump:callBar];
-	
-	NSArray *buttons = [callBar visibleButtons];
-	
-//	for (id button in buttons) {
-//		DNSLog(@"Button %@ %@", NSStringFromSelector([button action]), button);
-//	}
-	if ([callBar visible] && [callBar recentlyFaded]) {
-	}
-	
 	if (![callBar visible] && ![callBar recentlyFaded]) {
-		NSArray *buttons = [callBar visibleButtons];
-		
-		if (targetClassArray == nil) {
-			targetClassArray = [[NSMutableArray array] retain];
-		}
-		[targetClassArray removeAllObjects];
-		
-		if (editedButtons == nil) {
+		// make array to save buttons which are replaced captions or images.
+		if (editedButtons == nil)
 			editedButtons = [[NSMutableArray array] retain];
-		}
-		else {
-		}
+		else
+			[editedButtons removeAllObjects];
 		
-		for (int i = 0; i < [buttons count]; i++) {
-			id button = [buttons objectAtIndex:i];
+		// If target class has method to replace UICallBarButton's caption, do it.
+		for (id button in [callBar visibleButtons]) {
 			SEL action = [button action];
 			id target = [callBar targetForAction:action];
 			if ([[target class] respondsToSelector:@selector(replaceCallBarButton:action:)]) {
 				[[target class] replaceCallBarButton:button action:action];
-				[targetClassArray addObject:[target class]];
 				[editedButtons addObject:button];
 			}
 		}
 	}
 }
 
+// Call this method when UICallBar's animation ends appearing and hiding. 
 - (void)calloutBarDidFinishAnimation:(id)callBar {
 	DNSLogMethod
-	[self checkMethod:callBar];
-//	[self dump:callBar];
-	
-	BOOL isDirty = NO;
 	if (![callBar visible] && [callBar recentlyFaded]) {
-		
-		
+		// Get back buttons with original states.
 		for (id button in [editedButtons reverseObjectEnumerator]) {
-			DNSLog(@"Button %@ %@", NSStringFromSelector([button action]), button);
-		}
-		
-		for (id button in [editedButtons reverseObjectEnumerator]) {
-			SEL action = [button action];
-			// id target = [callBar targetForAction:action];
-			[self getBacckCallBarButton:button action:action];
-			/*
-			if ([[target class] respondsToSelector:@selector(getBacckCallBarButton:action:)]) {
-				[[target class] getBacckCallBarButton:button action:action];
-				DNSLog(@"Button %@ %@", NSStringFromSelector([button action]), button);
-				isDirty = YES;
-			}
-			 */
-			isDirty = YES;
+			[self getBackCallBarButton:button action:[button action]];
 			[editedButtons removeObject:button];
-		}
-		
-		if (isDirty) {
-			[[NSNotificationCenter defaultCenter] postNotificationName:kDidDismissedUIMenuController object:nil];
 		}
 	}
 }
 
 @end
-
-/*
-for (int i = 0; i < [buttons count]; i++) {
-	id button = [buttons objectAtIndex:i];
-	if ([targetClassArray count]) {
-		id targetClass = [targetClassArray objectAtIndex:i];
-		SEL action = [button action];
-		NSLog(@"%@", NSStringFromSelector(action));
-		if ([targetClass respondsToSelector:@selector(getBacckCallBarButton:action:)]) {
-			[targetClass getBacckCallBarButton:button action:action];
-			isDirty = YES;
-		}
-	}
-}
-*/
